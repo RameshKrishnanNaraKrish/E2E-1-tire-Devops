c1809b5aef8a82ea9d5cac05327fcef353e80e0e
Rameshkrishnannarashimankrishnamurthy <>
2024-07-09 21:39:05 -0400

    modified

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl"
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
